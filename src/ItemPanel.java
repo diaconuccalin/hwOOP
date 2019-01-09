65eb8cbf12030496bbd0b10fe6a7e24ccc556ac4
Calin Diaconu <diaconuccalin@gmail.com>
2019-01-09 16:55:02 +0200

    Removed unnecessary imports

--- src/ItemPanel.java
+++ src/ItemPanel.java
@@ -1,9 +1,6 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.beans.PropertyChangeListener;
 
 public class ItemPanel extends Frame{
     public ItemPanel(String panelName) {
